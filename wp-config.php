996f8aaf1b4735571d057505d719faca7aa8965a
paragbharne <pari.bharne@gmail.com>
2016-12-19 08:53:14 +0000

    initial

--- wp-config.php
+++ wp-config.php
@@ -22,10 +22,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'prod');
+define('DB_NAME', 'prod1');
 
 /** MySQL database username */
-define('DB_USER', 'prod');
+define('DB_USER', 'prod1');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'password');
