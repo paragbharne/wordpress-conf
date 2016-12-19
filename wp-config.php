091c5e5bdec62b7c1fff35a764d30ee905dd122a
paragbharne <pari.bharne@gmail.com>
2016-12-19 10:08:45 +0000

    iniital Comiit

--- wp-config.php
+++ wp-config.php
@@ -22,10 +22,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'dev1');
+define('DB_NAME', 'dev');
 
 /** MySQL database username */
-define('DB_USER', 'dev1');
+define('DB_USER', 'dev');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'password');
